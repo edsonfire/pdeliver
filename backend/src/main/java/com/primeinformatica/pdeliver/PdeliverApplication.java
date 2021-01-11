205d28ecdc80501194c9a53d88c9c600256c49b6
Edson Mesquita <edsonfire@gmail.com>
2021-01-11 09:40:42 -0300

    proejto criado

---
+++ backend/src/main/java/com/primeinformatica/pdeliver/PdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.primeinformatica.pdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/primeinformatica/pdeliver/PdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeinformatica.pdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
