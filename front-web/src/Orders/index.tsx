55893638989379d153da3f789facb22fcb045e4f
unknown <edsonfire@gmail.com>
2021-01-23 12:28:08 -0300

    2301

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -4,13 +4,14 @@ import StepsHeader from './StepsHeader';
 import ProductsList from './ProductsList';
 import { Product } from './Types';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
 
 
 
 function Orders(){
 const [products, setProducts] = useState<Product[]>([])
-       
+const [orderLocation, setOrderLocation]   
         useEffect(()=>{
                         fetchProducts()
                         .then(reponse => setProducts(reponse.data))
@@ -25,7 +26,7 @@ const [products, setProducts] = useState<Product[]>([])
               
                 <StepsHeader></StepsHeader>
                 <ProductsList  products={products} ></ProductsList>
-
+                <OrderLocation></OrderLocation>
 
         </div>
 
